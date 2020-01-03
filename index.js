2bbcc2dd6c63830187280e582f42c0e7b17e5209
UtoiShiha <ameken_shiha@yahoo.co.jp>
2020-01-03 20:49:37 +0900

    Commit All Files

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ const path=require('path');
 const express=require('express');
 const app=express();
 
+
+
 // ポートはheroku用の環境変数とローカル用の手動を設定しておく
 app.set('port', process.env.PORT || 5050);
 // '/public/'以下を静的ファイルの置き場所に指定
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-const log=require('./src/log');
-app.use(log.access.add); // accessログに記録するためのミドルウェア
-
-// JSON形式でリソースログを返すAPI
-app.get('/log/usage', (req, res)=>{ res.send(log.usage.get()) }); 
-// JSON形式でアクセスログを返すAPI
-app.get('/log/access', (req, res)=>{ res.send(log.access.get()) }); 
