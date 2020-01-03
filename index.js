b7e1f8bb2dd65e92fab9d8335e1031aed5a24e49
UtoiShiha <ameken_shiha@yahoo.co.jp>
2020-01-03 20:43:00 +0900

    Commit All Files

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ app.set('port', process.env.PORT || 5050);
 // '/public/'以下を静的ファイルの置き場所に指定
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), ()=>{ console.log("Node app is running at localhost:" + app.get('port')); });
+
+const log=require('./src/log');
+app.use(log.access.add); // accessログに記録するためのミドルウェア
+
+// JSON形式でリソースログを返すAPI
+app.get('/log/usage', (req, res)=>{ res.send(log.usage.get()) }); 
+// JSON形式でアクセスログを返すAPI
+app.get('/log/access', (req, res)=>{ res.send(log.access.get()) }); 
