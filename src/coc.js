1305ff5f4b58c78c672f1037145c7dd3fd365b7e
UtoiShiha <ameken_shiha@yahoo.co.jp>
2020-01-07 09:02:31 +0900

    リクエスト送信機能追加

--- src/coc.js
+++ src/coc.js
@@ -1,45 +1,46 @@
-let cocClients=[];
+module.exports = class CoC{
+    constructor(){
+        this.cocClients=[];
+        this.sessions=[];
+    }
+    server(ws,req){
+        // 通信してきた相手の情報がwsに入っている
+        console.log("open");
+        this.cocClients.push(ws);
 
-const chat=(ws, req)=>{
-    // 通信してきた相手の情報がwsに入っている
-    console.log("open");
-    cocClients.push(ws);
+        ws.on('message', message=>{
+            console.log("message");
+            const json=JSON.parse(message);
+            switch (json.method) {
+                case "entry":
+                    console.log("entry");
+                    this.cocClients.forEach(a=>{ if( a===ws && a.role==undefined && a.name==null) a.role=json.role });
+                    this.cocClients.forEach(a=>{ a.send(JSON.stringify(this.cocClients)); });
+                    break;
+                case "dice":
+                    console.log("dice");
+                    let dice=json.dice;
+                    this.cocClients.filter(a=>{
+                        return a.role == "player";
+                    }).forEach(a=>{
+                        a.send(JSON.stringify(json));
+                    });
+                    break;
+                case "dice/response":
+                    break;
+                default:
+                    break;
+            }
 
-    ws.on('message', message=>{
-        console.log("message");
-        const json=JSON.parse(message);
-        switch (json.method) {
-            case "entry":
-                console.log("entry");
-                entry();
-                cocClients.forEach(a=>{ if( a===ws && a.role==undefined && a.name==null) a.role=json.role });
-                cocClients.forEach(a=>{ a.send(JSON.stringify(cocClients)); });
-                break;
-            case "dice":
-                console.log("dice");
-                dice=json.dice;
-                cocClients.filter(a=>{
-                    return a.role == "player";
-                }).forEach(a=>{
-                    a.send(JSON.stringify(json));
-                });
-                break;
-            default:
-                break;
-        }
-
-        if( json.method!=null ) json.clients=cocClients.map(a=> a.name);
+            if( json.method!=null ) json.clients=this.cocClients.map(a=> a.name);
 
 
-    });
-    ws.on('close', ()=>{    
-        console.log("close");
-        const name=cocClients.find(a=> a===ws).role;
-        cocClients=cocClients.filter(a=> a!==ws);
-        cocClients.forEach(a=>{ a.send(JSON.stringify({ user: name, method: 'close', clients: cocClients.map(a=>name) })); });
-    });
+        });
+        ws.on('close', ()=>{    
+            console.log("close");
+            const name=this.cocClients.find(a=> a===ws).role;
+            this.cocClients=this.cocClients.filter(a=> a!==ws);
+            this.cocClients.forEach(a=>{ a.send(JSON.stringify({ user: name, method: 'close', clients: this.cocClients.map(a=>name) })); });
+        });
+    }
 }
-const entry = function(){
-    console.log("test");
-}
-module.exports=chat;
